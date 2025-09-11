efc39a3f4ecd8a03620d399a61b952951c8c298e
Sanderson Martins Cadoso <143949799+sandersoncardoso1980@users.noreply.github.com>
2025-09-11 06:55:11 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
+// substitui "lexis-ai-desk" pelo nome do teu repo
+const repoName = "/lexis-ai-desk/";
+
 export default defineConfig(({ mode }) => ({
+  base: mode === "development" ? "/" : repoName,  // <--- aqui
   server: {
     host: "::",
     port: 8080,
