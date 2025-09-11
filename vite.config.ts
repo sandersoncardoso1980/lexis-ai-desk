14abd6bdb821cc9f3920874ec4bf7fabde0222ab
Sanderson Martins Cadoso <143949799+sandersoncardoso1980@users.noreply.github.com>
2025-09-11 07:07:50 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // substitui "lexis-ai-desk" pelo nome do teu repo
-const repoName = "/lexis-ai-desk/";
+const repoName = lexis-ai-desk;
 
 export default defineConfig(({ mode }) => ({
   base: mode === "development" ? "/" : repoName,  // <--- aqui
